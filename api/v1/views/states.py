b94390a71b50b9b18e01a3930e4bb7cf85615e21
MarvelousMarvin7 <marvinagyei3@gmail.com>
2024-08-09 17:31:19 +0000

    updated

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -38,9 +38,9 @@ def delete_state(state_id):
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
     """Creates a State"""
-    if not request.json:
+    if not request.get_json():
         abort(400, description="Not a JSON")
-    if 'name' not in request.json:
+    if 'name' not in request.get_json():
         abort(400, description="Missing name")
     data = request.get_json()
     new_state = State(**data)
